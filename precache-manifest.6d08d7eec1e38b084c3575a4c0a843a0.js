59383177e9493c54508a9686456c063425975d43
K C Gaurab <s1900524@edu.bc.fi>
2019-09-08 17:41:27 +0300

    Updates

---
+++ precache-manifest.6d08d7eec1e38b084c3575a4c0a843a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101a6e9beae8947e4e54441ffd7218b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1e2799056040701a8a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58a1efbf369f00632ca0",
+    "url": "/static/js/2.0249e9c2.chunk.js"
+  },
+  {
+    "revision": "9c1e2799056040701a8a",
+    "url": "/static/js/main.37213d60.chunk.js"
+  },
+  {
+    "revision": "b8e6ccbaa1bd44afc798",
+    "url": "/static/js/runtime~main.67a5e9ca.js"
+  }
+]);
